9655a1e6aab1db0b06c28549cf340d40189cba1f
Roger Santos <roger.santos36@gmail.com>
2022-04-28 15:34:49 -0300

    bee-1013

---
+++ bee-1013/script.js
@@ -0,0 +1,11 @@
+// ÁREA
+
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+const values = input.split(/\s\n?/).map(number => Number(number))
+
+const orderedNumbers = values.sort((item1, item2) => item1 - item2)
+const higherNumber = orderedNumbers[orderedNumbers.length - 1]
+
+const resultMessage = `${higherNumber} eh o maior`
+console.log(resultMessage)
