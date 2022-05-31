85687678b63da9173a15a380e08956466aad1f1c
Roger Santos <roger.santos36@gmail.com>
2022-05-31 14:50:43 -0300

    :sparkles: feat: update probleam description

--- bee-1019/script.js
+++ bee-1019/script.js
@@ -1,5 +1,15 @@
 // CONVERSÃO DE TEMPO
 
+/*
+  Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+  Entrada
+  A entrada contém 2 valores inteiros.
+
+  Saída
+  Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenha um espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+*/
+
 var input = require('fs').readFileSync('stdin', 'utf8');
 
 const value = Number(input.split())
